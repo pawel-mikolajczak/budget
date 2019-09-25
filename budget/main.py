86a72941d75f4b3c47df9734746ba6a2efb6fea7
pabllo.pl <pabllo.pl@gmail.com>
2019-09-25 12:50:04 +0200

    Inline processes

--- budget/main.py
+++ budget/main.py
@@ -25,16 +25,11 @@ def main():
     wydatki = ws.process_wydatki(excel_file_path)
     ws.store_wydatki(wydatki, database)
 
-    wydatki_miesiace = ws.process_miesiace(database)
-    wydatki_kategorie = ws.process_kategorie(database)
-    wydatki_subkategorie = ws.process_subkategorie(database)
-    wydatki_sum = ws.process_sum_wydatki(database)
-
     ex.save_wydatki_to_excel(wydatki)
-    ex.save_data_to_excel(wydatki_miesiace, ["Miesiąc"], "Miesiące")
-    ex.save_data_to_excel(wydatki_kategorie, ["Kategoria"], "Kategorie")
-    ex.save_data_to_excel(wydatki_subkategorie, ["Kategoria", "Subkategoria"], "Subkategorie")
-    ex.save_data_to_excel(wydatki_sum, ["Miesiąc", "Suma"], "Wydatki SUM")
+    ex.save_data_to_excel(ws.process_miesiace(database), ["Miesiąc"], "Miesiące")
+    ex.save_data_to_excel(ws.process_kategorie(database), ["Kategoria"], "Kategorie")
+    ex.save_data_to_excel(ws.process_subkategorie(database), ["Kategoria", "Subkategoria"], "Subkategorie")
+    ex.save_data_to_excel(ws.process_sum_wydatki(database), ["Miesiąc", "Suma"], "Wydatki SUM")
 
     ex.closeExcel()
     database.closeConnection()
