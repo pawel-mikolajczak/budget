ad86f9f6eee02a046a82d9c023eea1f463db35a5
pabllo.pl <pabllo.pl@gmail.com>
2019-09-29 20:55:32 +0200

    Improved logging for AccountsService

--- budget/accounts/AccountsService.py
+++ budget/accounts/AccountsService.py
@@ -26,13 +26,14 @@ class AccountsService:
     def process_items(self, input_file_path):
         xslx = pd.ExcelFile(input_file_path)
 
-        items = []
+        items = list()
 
         for konto in konta:
             logger.info("Processing account: '{}' started...".format(konto))
             records_processed = self.process_account(xslx, konto, items)
-            items.__add__(records_processed)
-            logger.info("Processing account: '{}' finished. Number of records: {}".format(konto, records_processed.__len__()))
+            items.extend(records_processed)
+            logger.info(
+                "Processing account: '{}' finished. Number of records: {}".format(konto, records_processed.__len__()))
 
         return items
 
@@ -48,9 +49,11 @@ class AccountsService:
 
         return accounts
 
-    def store_konta(self, konta, database):
+    def store_konta(self, konta:list, database):
+        logger.info("Storing accounts to database: {}...".format(konta.__len__()))
         for konto in konta:
             database.add_konto(konto.typ, konto.data, konto.opis, konto.kwota, konto.bilans)
+        logger.info("Storing accounts to database finished: {}...".format(konta.__len__()))
 
     def process_sum_konta(self, database):
         return database.select_data("SELECT "
